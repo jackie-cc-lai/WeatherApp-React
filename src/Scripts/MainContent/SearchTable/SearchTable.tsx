f4f36f93ad20796a3133a9307184893f2c01e350
Jackie <jackielai_123@hotmail.com>
2021-01-16 10:51:22 -0500

    Added another grid layer to make it more maneuverable in responsiveness

--- src/Scripts/MainContent/SearchTable/SearchTable.tsx
+++ src/Scripts/MainContent/SearchTable/SearchTable.tsx
@@ -7,6 +7,7 @@ import { Grid } from '@material-ui/core';
 
 interface SearchState{
     data:Array<any>;
+    selectAll:boolean;
 }
 
 interface PassedProps extends React.Props<any>{
@@ -17,7 +18,8 @@ export class SearchTable extends React.Component<PassedProps,SearchState>{
     constructor(props:any){
         super(props);
         this.state = {
-            data:this.props.data
+            data:this.props.data,
+            selectAll:false
         }
         console.log(this.state.data);
     };
@@ -31,14 +33,30 @@ export class SearchTable extends React.Component<PassedProps,SearchState>{
     selectCity = async (city:any) =>{
         await this.props.callback(city);
     }
+    addCity = async (city:any) =>{
+        console.log(city);
+        let cities = this.state.data;
+        let citiesFound = cities.find(r=>r.id == city.id);
+        citiesFound.isSelected = !citiesFound.isSelected;
+        this.setState({data:cities});
+    }
+
+    selectAll = async () =>{
+        
+        let cities = this.state.data;
+        cities.forEach((c)=>{
+            c.isChecked = !this.state.selectAll;
+        })
+        this.setState({data:cities, selectAll:!this.state.selectAll});
+    }
 
     renderTable:any = () =>{
         return this.state.data.map((r,i)=>{
             return (
-                <Grid item alignItems="center" direction="row" justify="space-around" lg={12} md={12} xs={12} className="searchIndiv" key={i}>
-                    <div onClick={()=>{this.selectCity(r)}}>
+                <Grid container alignItems="center" direction="row" justify="space-around" lg={12} md={12} xs={12} className="searchIndiv" key={i}>
+                    <Grid item lg={12} xs={12} onClick={()=>{this.selectCity(r)}}>
                         {`${r.name}${r.state ? ',' : ''} ${r.state}, ${r.country}`}
-                    </div>
+                    </Grid>
                 </Grid>
             )
         })
@@ -46,7 +64,12 @@ export class SearchTable extends React.Component<PassedProps,SearchState>{
 
     render(){
         return(
-            <Grid container direction="row" justify="space-evenly" alignItems="center" spacing={1} className="forecastTable">{this.renderTable()}</Grid>
+            <Grid container direction="row" justify="space-evenly" alignItems="center" spacing={1} className="forecastTable">
+                <Grid item lg={12} xs={12}>
+                    <input type="checkbox" onClick={()=>{this.selectAll()}} />
+                </Grid>
+                {this.renderTable()}
+            </Grid>
         )
     }
 }
